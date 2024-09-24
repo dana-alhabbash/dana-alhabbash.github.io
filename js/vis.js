791b580fc2be9652ac2cef5727d7605afb5ed727
dana-alhabbash <danalhabbash@gmail.com>
2024-09-23 17:12:10 -0700

    animation using vis.js

---
+++ js/vis.js
@@ -0,0 +1,20 @@
+//flag animation
+let triangle = document.getElementById('triangle');
+let growing = true;
+let size = 100; 
+
+function animateFlag() {
+    if (growing) {
+        size += 3;
+        if (size >= 110) { 
+            growing = false;
+        }
+    } else {
+        size -= 2;
+        if (size <= 90) { 
+            growing = true;
+        }
+    }
+    triangle.setAttribute("points", `0,0 ${size},100 0,200`);
+}
+setInterval(animateFlag, 100);
